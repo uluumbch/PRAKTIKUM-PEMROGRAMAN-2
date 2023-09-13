ed7859c453393e1590d8db89b9c77c43d995762b
uluumbch <uluum123@gmail.com>
2023-09-13 16:47:37 +0800

    UPLOAD PERTAMA

---
+++ PRAKTIKUM1/src/PRAK105_2020817210025_BachrulUluum.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class PRAK105_2020817210025_BachrulUluum {
+    public static void main(String []args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukan Makanan Favorit: ");
+        String makanan_favorit = input.nextLine();
+        System.out.println("makanan fav " + makanan_favorit);
+    }
+}
